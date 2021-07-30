191790327679eebee0922b4dbeb6591e13eec463
Madness-D <62844021+Madness-D@users.noreply.github.com>
2021-07-30 22:03:30 +0800

    出租车费 未ac

---
+++ 4 算法初步/4.4 贪心/B 出租车费.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+double fee(double distance){
+    double fee=0;
+    if(distance<=4)
+        fee=10;
+    if(distance>4 && distance<=8)
+        fee=10+2*(distance-4.0);
+    if(distance>8){
+        while(distance>=8){
+            fee+=18;
+            distance-=8;
+        }
+        if(distance <=4)
+            fee+= 2.4*distance;
+        else
+            fee+=10+2*(distance-4.0);
+    }
+    return fee;
+}
+
+int main(){
+    int n;
+    while(cin>>n && n){
+        if(fee(n)==(int)fee(n))
+            cout<<fee(n)<<endl;
+        else    
+            printf("%.1f\n",fee(n));
+    }
+    return 0;
+}
+
