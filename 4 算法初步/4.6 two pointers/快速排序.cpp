1c4d1d8b1e4fd006ac1e9276bef4413aa1fbe280
Madness-D <62844021+Madness-D@users.noreply.github.com>
2021-08-03 12:09:21 +0800

    快排

---
+++ 4 算法初步/4.6 two pointers/快速排序.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+int Partition(int res[], int i, int j) { 
+    int left = i, right = j;
+    while(left < right)
+    {
+        while(left < right && res[i] <= res[right])
+            right --;
+        while(left < right && res[i] > res[left])
+            left ++;
+        swap(res[left], res[right]);
+    }
+    swap(res[i], res[left]);
+    return left;
+
+}
+
+void qsort(int A[], int left, int right) {
+    if (left < right) {
+        int pos = Partition(A, left, right);
+        qsort(A, left, pos - 1);
+        qsort(A, pos + 1, right);
+    }
+}
+
+int main() {
+   // srand((unsigned)time(NULL));
+    int n;
+    int a[5050];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+            cin >> a[i];
+     qsort(a, 0, n-1);
+    for (int i = 0; i < n; i++)
+            cout << a[i] << "\n";
+    
+    return 0;
+}
