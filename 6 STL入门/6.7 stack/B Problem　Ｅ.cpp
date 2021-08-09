cf551e8e822e9c603ef3dd7425dddcaa1196f6e2
Madness_D <dingbuowen@outlook.com>
2021-08-09 19:20:12 +0800

    problem e 添加注释

--- 6 STL入门/6.7 stack/B Problem　Ｅ.cpp
+++ 6 STL入门/6.7 stack/B Problem　Ｅ.cpp
@@ -26,18 +26,21 @@ void fun2(string str) {
         if (str[i] == '(' || str[i] == '{' || str[i] == '[') {//左半边，入栈
             st.push(str[i]);
         }
-        if (str[i] == ')' || str[i] == '}' || str[i] == ']') {
-            if (st.empty()) { flag = false; break; }
+        if (str[i] == ')' || str[i] == '}' || str[i] == ']') {//右半边，和栈中的元素比较匹配
+            if (st.empty()) { // 栈空，无法匹配
+                flag = false; break;
+                 }
             else {
-                flag = fun1(st.top(), str[i]);
-                if (flag && !st.empty()) { st.pop(); }
-                if (!flag) { break; }
+                flag = fun1(st.top(), str[i]);//返回匹配结果
+                if (flag ) { //匹配得上，出栈
+                    st.pop(); }
+                if (!flag) { //匹配不上，后面的也就不用比了
+                    break; }
             }
-
         }
-
     }
-    if(!st.empty()){flag=false;}
+
+    if(!st.empty()){flag=false;}// 第一次未考虑到这种情况，如只输入[，这样的话经过上面的循环，flag仍为true
     if (!flag) { cout << "no\n"; }
     if (flag) { cout << "yes\n"; }
 }
@@ -45,7 +48,7 @@ void fun2(string str) {
 int main() {
     int n;
     while (cin >> n && n) {
-        getchar();
+        getchar();//如果没有这个，在输入n的时候也直接输出yes了
         while (n--) {
             string temp;
             getline(cin, temp);
