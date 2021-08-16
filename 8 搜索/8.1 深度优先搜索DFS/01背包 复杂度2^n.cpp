23fde2ab40162cf045eb854053e6c4b30f9ee47d
Madness-D <62844021+Madness-D@users.noreply.github.com>
2021-08-16 11:46:26 +0800

    背包

---
+++ 8 搜索/8.1 深度优先搜索DFS/01背包 复杂度2^n.cpp
@@ -0,0 +1,36 @@
+/*
+岔道口：对于每件物品，选择放入背包还是不选
+死胡同：选择的物品重量总和超过背包容量V，返回最近的岔道口
+*/
+
+#include<cstdio>
+
+const int maxn=30;
+int n,v,maxValue=0;//物品件数n，背包容量v，最大价值maxValue
+int w[maxn],c[maxn];//重量 体积
+//DFS函数，index为当前处理的物品编号
+//sumW和sumC为当前的总重量和总价值
+void DFS(int index,int  sumW,int sumC){
+    if(index==n){
+        if(sumW<=v && sumC> maxValue){
+            maxValue=sumC;
+        }
+        return ;
+    }
+    //岔道口，两种选择,
+    DFS(index+1,sumW,sumC);
+    DFS(index+1,sumW+w[index],sumC+c[index]);
+}
+
+int main(){
+    scanf("%d %d",&n,&v);
+    for(int i=0;i<n;i++){
+        scanf("%d",&w[i]);
+    }
+    for(int i=0;i<n;i++){
+        scanf("%d",&c[i]);
+    }
+    DFS(0,0,0);
+    printf("%d\n",maxValue);
+    return 0;
+}
